ccf0ea63f574c0552787185624ae1393182b5da7
Malla Raraju <72393778+MallaRaraju@users.noreply.github.com>
2023-07-16 17:49:46 +0530

    Update DataAugPl.py

--- DataAugPl.py
+++ DataAugPl.py
@@ -22,7 +22,7 @@ class KP_augmentation():
                         A.augmentations.geometric.transforms.Affine(p=0.3,scale=[0.25,0.75]),
                         A.augmentations.geometric.transforms.Affine(p=0.3,scale=[1.25,1.75]),
                         A.augmentations.geometric.transforms.Affine(p=0.3,rotate=[-15,15]) ],
-                       keypoint_params=A.KeypointParams(format='xy', label_fields=['class_labels']),)
+                       keypoint_params=A.KeypointParams(format='xy', label_fields=['point_labels','image_labels' ]),)
     
     def aug_pl(self, label_dir):
         labels = os.listdir(label_dir)
@@ -36,20 +36,23 @@ class KP_augmentation():
                 stuff = annot['shapes']
                 img_path = os.path.join('/',root_dir,'data', 'image',f'{label.split(".")[0]}.jpg')
                 img = cv2.imread(img_path)
-                if len(stuff)>1:
+                if len(stuff)>2:
                     coords = [tuple(stuff[0]['points'][0]),tuple(stuff[1]['points'][0])]
                     classes = [stuff[0]['label'], stuff[1]['label']]
+                    img_type = [stuff[2]['label']]
                 else:
                     coords = [tuple(stuff[0]['points'][0])]
                     classes = [stuff[0]['label']]
+                    img_type = [stuff[1]['label']]
                 os.remove(img_path)
                 os.remove(os.path.join(label_dir,label))
                 for i in range(60):
                     annotation = {}
-                    new_aug = self.augmentor(image=img,keypoints=coords,class_labels=classes)
+                    new_aug = self.augmentor(image=img,keypoints=coords,point_labels=classes, image_labels = img_type)
                     annotation['image']=f'{img_path.split(".")[0]}_{i}.jpg'
                     annotation['points']=new_aug['keypoints']
-                    annotation['labels']=new_aug['class_labels']
+                    annotation['point_labels']=new_aug['point_labels']
+                    annotation['image_label']=new_aug['image_labels']
                     cv2.imwrite(f'{img_path.split(".")[0]}_{i}.jpg',new_aug['image'])
                     with open(os.path.join('/',root_dir,'data','label',f'{label.split(".")[0]}_{i}.json'), 'w') as o:
                         json.dump(annotation,o)
