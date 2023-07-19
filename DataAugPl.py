be6da815cc71ab9f564b78f1fb88d70cf7efa41e
Malla Raraju <72393778+MallaRaraju@users.noreply.github.com>
2023-07-19 19:27:55 +0530

    Update DataAugPl.py

--- DataAugPl.py
+++ DataAugPl.py
@@ -46,5 +46,5 @@ class KP_augmentation():
                     annotation['point_labels']=new_aug['point_labels']
                     annotation['image_label']=new_aug['image_labels'][0]
                     cv2.imwrite(f'{img_path.split(".")[0]}_augmented_{i}.jpg',new_aug['image'])
-                    with open(os.path.join('/',root_dir,'data','label',f'{label.split(".")[0]}_augmentged_{i}.json'), 'w') as o:
+                    with open(os.path.join(label_dir, f'{label.split(".")[0]}_augmentged_{i}.json'), 'w') as o:
                         json.dump(annotation,o)
